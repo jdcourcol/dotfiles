40e162300c655f8a13a446627ab2fbd70b310f62
courcol <jean-denis.courcol@epfl.ch>
2017-08-22 11:38:33 +0200

    enable mouse copy in term.

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -329,6 +329,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (xterm-mouse-mode -1)
   (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
   (setq vc-follow-symlinks t)
   (setq evil-want-fine-undo 'fine)
