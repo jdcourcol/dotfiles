7ace68be713d1c8350c9bcd735a5d3af2ee3b1b0
jean-denis courcol <jean-denis.courcol@epfl.ch>
2022-02-16 13:45:17 +0100

    adding function to archive done tasks

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -68,7 +68,8 @@ values."
      org
      mu4e
      (mu4e :variables
-           mu4e-installation-path "/usr/local/share/emacs/site-lisp/mu4e/")
+           mu4e-installation-path "/usr/local/share/emacs/site-lisp/mu4e/"
+           )
      osx
      ranger
      ;; (shell :variables
@@ -333,6 +334,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
   ;; (setq shell-default-term-shell "/bin/zsh")
   (setq avy-all-windows 'all-frames)
   (setq ispell-program-name "aspell")
+  (setq mu4e-mu-binary "/usr/local/bin/mu")
   )
 
 (defun dotspacemacs/user-config ()
@@ -347,6 +349,17 @@ you should place your code here."
   (add-hook 'org-mode-hook
             (lambda()
               (setq show-trailing-whitespace nil)))
+  (setq org-refile-targets '((nil :maxlevel . 2)
+                             ))
+  (defun org-archive-done-tasks ()
+    (interactive)
+    (org-map-entries
+     (lambda ()
+       (org-archive-subtree)
+       (setq org-map-continue-from (org-element-property :begin (org-element-at-point))))
+     "/DONE" 'file))
+  ;; performance https://orgmode.org/worg/agenda-optimization.html
+  (setq org-agenda-use-tag-inheritance nil)
   (defun thunderbird-open (msgid)
     "open msgid in thunderbird"
     (setq debug-on-error 't)
@@ -560,8 +573,7 @@ This function is called at the very end of Spacemacs initialization."
  ;; If there is more than one, they won't work right.
  '(evil-want-Y-yank-to-eol nil)
  '(package-selected-packages
-   (quote
-    (graphviz-dot-mode log4e gntp json-snatcher json-reformat multiple-cursors parent-mode gitignore-mode marshal logito pcache flyspell-correct pos-tip flx ghub treepy graphql iedit anzu tide typescript-mode edit-server org-mime yasnippet async websocket gh company avy ht alert magit-popup with-editor hydra f winum vue-mode use-package osx-dictionary live-py-mode hy-mode helm-make evil-nerd-commenter evil-matchit dumb-jump column-enforce-mode dash-functional smartparens evil flycheck projectile magit org-plus-contrib helm helm-core js2-mode dash s yapfify yaml-mode ws-butler which-key web-mode web-beautify vue-html-mode volatile-highlights vi-tilde-fringe uuidgen toc-org tagedit ssass-mode spaceline smeargle slim-mode scss-mode sass-mode reveal-in-osx-finder restclient-helm restart-emacs ranger rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode popwin pkg-info pip-requirements persp-mode pcre2el pbcopy paradox ox-reveal osx-trash orgit org-projectile org-present org-pomodoro org-download org-bullets open-junk-file ob-restclient ob-http neotree mu4e-maildirs-extension mu4e-alert move-text mmm-mode magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum livid-mode linum-relative link-hint less-css-mode launchctl keyfreq json-mode js2-refactor js-doc jinja2-mode info+ indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag goto-chg google-translate golden-ratio gnuplot github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-commit gist gh-md fuzzy flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fasd fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-terminal-cursor-changer evil-surround evil-snipe evil-search-highlight-persist evil-numbers evil-mc evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav ein edit-indirect diminish cython-mode csv-mode company-web company-tern company-statistics company-restclient company-ansible company-anaconda coffee-mode clean-aindent-mode bind-key auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile ansible-doc ansible aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell)))
+   '(dap-mode bui log4e gntp json-snatcher json-reformat multiple-cursors parent-mode gitignore-mode marshal logito pcache flyspell-correct pos-tip flx ghub treepy graphql iedit anzu tide typescript-mode edit-server org-mime yasnippet async websocket gh company avy ht alert magit-popup with-editor hydra f winum vue-mode use-package osx-dictionary live-py-mode hy-mode helm-make evil-nerd-commenter evil-matchit dumb-jump column-enforce-mode dash-functional smartparens evil flycheck projectile magit org-plus-contrib helm helm-core js2-mode dash s yapfify yaml-mode ws-butler which-key web-mode web-beautify vue-html-mode volatile-highlights vi-tilde-fringe uuidgen toc-org tagedit ssass-mode spaceline smeargle slim-mode scss-mode sass-mode reveal-in-osx-finder restclient-helm restart-emacs ranger rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode popwin pkg-info pip-requirements persp-mode pcre2el pbcopy paradox ox-reveal osx-trash orgit org-projectile org-present org-pomodoro org-download org-bullets open-junk-file ob-restclient ob-http neotree mu4e-maildirs-extension mu4e-alert move-text mmm-mode magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum livid-mode linum-relative link-hint less-css-mode launchctl keyfreq json-mode js2-refactor js-doc jinja2-mode info+ indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag goto-chg google-translate golden-ratio gnuplot github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-commit gist gh-md fuzzy flyspell-correct-helm flycheck-pos-tip flx-ido fill-column-indicator fasd fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-terminal-cursor-changer evil-surround evil-snipe evil-search-highlight-persist evil-numbers evil-mc evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav ein edit-indirect diminish cython-mode csv-mode company-web company-tern company-statistics company-restclient company-ansible company-anaconda coffee-mode clean-aindent-mode bind-key auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile ansible-doc ansible aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))
  '(paradox-github-token t)
  '(python-indent-guess-indent-offset nil)
  '(smtpmail-smtp-server "mail.epfl.ch")
